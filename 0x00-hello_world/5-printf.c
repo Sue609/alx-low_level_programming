74e5e6ea63861df83e23c36ad2e1c5942161a61c
Sue609 <susankamau609@gmail.com>
2023-02-16 03:39:07 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints size of various lines using printf function
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
