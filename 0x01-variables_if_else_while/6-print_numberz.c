76e0939ce6731c3cab2ddbac848097c6d66c1d4d
Sue609 <susankamau609@gmail.com>
2023-02-17 14:29:41 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: 0 (Success)
+ */
+
+/* Prints lower case alphabets in reverse */
+
+int main(void)
+{
+	int n;
+
+	for (n = 88; n < 98; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
