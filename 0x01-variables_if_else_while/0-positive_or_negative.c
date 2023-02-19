d1c4d1854abe2fd5dd601babdc510c81b3b02dd9
Sue609 <susankamau609@gmail.com>
2023-02-19 09:12:08 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,25 +4,27 @@
 
  /**
  * main = Assign random number to variable n
+ * 
  * return: always 0
  */
 
 int main(void)
 {
 int n;
-srand(time(0);
-
-/* Enter code */
+srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 
+/* code here */
+
 if (n > 0)
 {
-printff("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 
 if (n == 0)
-{printf("%d is zero\n", n);
+{
+printf("%d is zero\n", n);
 }
 
 if (n < 0)
