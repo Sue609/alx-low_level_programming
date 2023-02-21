944f6e8ed324cd5d54c7213145ba3d2fd5ec2ab6
Sue609 <susankamau609@gmail.com>
2023-02-21 09:40:43 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,3 +13,43 @@ return (1);
 else
 return (0);
 }
+
+#include "main.h"
+
+
+
+/**
+
+ * main - check the code.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+
+{
+
+    int r;
+
+
+
+    r = _islower('H');
+
+    _putchar(r + '0');
+
+    r = _islower('o');
+
+    _putchar(r + '0');
+
+    r = _islower(108);
+
+    _putchar(r + '0');
+
+    _putchar('\n');
+
+    return (0);
+
+}
