3e65a8bb6d563942783770657124ea0a55d5bf3b
Sue609 <susankamau609@gmail.com>
2023-02-21 10:35:17 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,33 +4,7 @@
  * @c: the character to be checked
  * Returns: 1 if c is letter otherwise 0.
  */
-int_isalpha(int c)
+int _isalpha(int c)
 {
-return ((c >= 'a' && c <= 'z') || (c >= 'A' && C <= 'Z'))
-}
-
-4-main.c CODE
-
-
-#include "main.h"
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int r;
-
-r = _isalpha('H');
-_putchar(r + '0');
-r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
-_putchar(r + '0');
-r = _isalpha(';');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
