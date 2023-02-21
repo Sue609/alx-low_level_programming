dcdc9aedc50c41109de4ded7fb72f2aca916dcb0
Sue609 <susankamau609@gmail.com>
2023-02-21 01:49:09 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Printing alphabet in lowercase.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	/* enter your code */
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
