254e540b772e5f5ab9ccd1db39da66acf94b0fe8
Sue609 <susankamau609@gmail.com>
2023-02-22 02:15:23 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
+{
+int x, y, z;
+if (n >= 0 && n <= 15)
+{
+for (x = 0; x <= n; x++)
+{
+for (y = 0; y <= n; y++)
+{
+z = y * x;
+if (y == 0)
+{
+_putchar(z + '0');
+}
+else if (z < 10 && x != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(z + '0');
+}
+else if (z >= 10 && z < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((z / 10) + '0');
+_putchar((z % 10) + '0');
+}
+else if (z >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((z / 100) + '0');
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
+}
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -23,6 +23,5 @@ return (0);
 else if (n < 0)
 {
 _putchar('-');
-}
 return (-1);
 }
