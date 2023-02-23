5e19b67b9170ba856a05be03835fbbdda068a880
Sue609 <susankamau609@gmail.com>
2023-02-23 12:13:50 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,15 @@
 void more_numbers(void)
 {
 int i, j;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
-for (j = 0; j <= 14; j++)
+for (j = 0; j < 15; j++)
 {
 if (j >= 10)
-{
-break;
-}
-_putchar(j);
-}
-_putchar(i);
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
+}
+
