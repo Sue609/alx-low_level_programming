b64870055e5f70ad76874b004708d8c09564efad
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-02-23 17:37:14 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,26 +14,26 @@ void times_table(void)
                 for (y = 0; y <= 9; y++)
                 {
                         z = x * y;
-                        if (z > 9)
-                        {
-                                u = z % 10;
-                                d = (z - u) / 10;
-                                _putchar(44);
-                                _putchar(32);
-                                _putchar(d + '0');
-                                _putchar(u + '0');
-                        }
-                        else
-                        {
-                                if (y != 0)
-                                {
-                                        _putchar(44);
-                                        _putchar(32);
-                                        _putchar(32);
-                                }
-                                _putchar(z + '0');
-                        }
-                }
-                _putchar('\n');
-      }
+                        if ({
+u = z % 10;
+d = (z - u) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
+}
+else
+{
+if (y != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}
+}
+_putchar('\n');
+}
+              
 }
