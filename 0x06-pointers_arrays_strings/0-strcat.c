2e4456f05ed0a04b70fc2cce98d5a0205b431dfd
Sue609 <susankamau609@gmail.com>
2023-03-01 10:08:21 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+ * _strcat - concantinates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int k;
+int dest_len = 0;
+
+while (dest[dest_len] != '\0')
+{
+	dest_len++;
+}
+
+k = 0;
+while (src[k] != '\0')
+{
+	dest[dest_len + k] = src[k];
+	k++;
+}
+dest[dest_len + k] = '\0';
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+There are many operations that we can perform in the C string library.
+Some of these operations are:
+- Copying a string
+- concantinating a string
+- Determining the length of a string
+- Copmparing two strings
+We will look at all these fuctions from the C string libraray.
