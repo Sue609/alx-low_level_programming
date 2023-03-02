ee37482e89805f97244f119d34160f93cfc3224b
Sue609 <susankamau609@gmail.com>
2023-03-02 01:24:14 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,8 +2,8 @@
 
 /**
  * string_toupper - changes all lowercase letters to uppercase.
- *
- *
+ * @str: the string to convert
+ *Return: pointer to resulting string
  */
 
 char *string_toupper(char *str)
@@ -11,11 +11,12 @@ char *string_toupper(char *str)
 int i = 0;
 while (str[i] != '\0')
 {
-	if (str[i] >= 'a' && str[i] <= 'z'){
-		str[i] = str[i] -32;
-	}
-	i++;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+i++;
 }
 
-return str;
+return (str);
 }
