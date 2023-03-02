7657162ffc7f32be2b88c8588b87e0c8c921a810
Sue609 <susankamau609@gmail.com>
2023-03-02 07:36:11 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,6 +8,13 @@ int *p;
 
 a[2] = 1024;
 p = &n;
+
+/**
+ * main - entry point
+ * Add one line of code
+ * Return: Always 0(success)
+ */
+
  /*
  * write your line of code here...
  * Remember:
@@ -16,10 +23,8 @@ p = &n;
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
+*(p + 5) = 98;
 
-/* ...so that this prints 98\n */
-
-*(p + 5) = 98;;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
