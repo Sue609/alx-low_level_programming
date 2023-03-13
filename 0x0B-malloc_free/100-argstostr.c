756a4a5022e2ef16b9e939594d372965dc7f303b
Sue609 <susankamau609@gmail.com>
2023-03-13 09:39:21 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concantinates all arguements.
+ * @ac: accepts an integer.
+ * @av: a pointer to another pointer.
+ *
+ * Return: pointer or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *concat = NULL;
+	int i, j;
+	int len;
+	int pos;
+
+	len = 0;
+	pos = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		len++;
+		}
+	}
+	len++;
+
+	concat = malloc(len * sizeof(char));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			concat[pos] = av[i][j];
+			pos++;
+		}
+		concat[pos] = '\n';
+
+		pos++;
+	}
+	concat[pos] = '\0';
+
+	return (concat);
+}
