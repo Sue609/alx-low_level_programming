b9fdaa0fa53959431c30eacbe5af91ef7461bc82
Sue609 <susankamau609@gmail.com>
2023-03-21 03:43:51 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: entry of the program.
+ * main - check the code
  *
- * Return: 0.
+ * Return: Always 0.
  */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return 0;
+	return (0);
 }
