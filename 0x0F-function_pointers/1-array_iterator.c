5fe67ee81453ee4109fd00edb55008f35c869667
Sue609 <susankamau609@gmail.com>
2023-03-22 07:13:35 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,8 +15,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || action == NULL)
+	return;
+
 	for (i = 0; i < size; i++)
 	{
-	(*action)(array[i]);
+	action(array[i]);
 	}
 }
