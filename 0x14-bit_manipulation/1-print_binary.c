a10d3d2387f150cac97a3ce7fa7f7010e8237eaf
Sue609 <susankamau609@gmail.com>
2023-04-06 08:07:53 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,15 +9,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-
-	while (mask > 0)
+	if (n >> 0)
 	{
-		if ((n & mask) == mask)
-			 _putchar('1');
-		else
-			_putchar('0');
+		if (n >> 1)
+			print_binary(n >> 1);
 
-		mask >>= 1;
+		_putchar((n & 1) + '0');
 	}
+		else
+		{
+			_putchar('0');
+		}
 }
