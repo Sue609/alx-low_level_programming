73c0d8ca495ecb16e3949eec091ce9c2edcb7764
Sue609 <susankamau609@gmail.com>
2023-04-09 13:30:01 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - function that creates a file.
+ * @filename: name of the file to create.
+ * @text_content: Null terminated string.
+ *
+ * Return: 1 or -1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		write(fd, text_content, strlen(text_content));
+	}
+	
+	close(fd);
+	
+	return (1);
+}
