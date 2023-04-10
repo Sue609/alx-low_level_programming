8568fab4a7fde1835be9aadaa064f14a5ec75062
Sue609 <susankamau609@gmail.com>
2023-04-10 01:00:12 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 		write(fd, text_content, strlen(text_content));
 
 	close(fd);
