e33d65213fa0c0404119e598cb141bd81802d4fd
Sue609 <susankamau609@gmail.com>
2023-04-10 04:34:29 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,16 +23,11 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	source = open(argv[1], O_RDONLY);
-	if (source == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, permissions);
-	if (destination == -1)
+	if (source == -1 || destination == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't open or create file(s).\n");
+		exit(98);
 	}
 	while ((ch = read(source, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -47,14 +42,9 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(source) == -1)
+	if (close(source) == -1 || close(destination) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source);
-		exit(100);
-	}
-	if (close(destination) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destination);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", errno);
 		exit(100);
 	}
 	return (0);
