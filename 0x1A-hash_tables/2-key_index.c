5b2ed24efe23599def7824f49981ce68c393bcdd
Sue609 <susankamau609@gmail.com>
2023-06-08 01:26:04 -0700

    The key_index function can be used to determine the index at which a key/value pair should be strored in the hash table array, allowing efficient access and retrieval of values based on their keys.

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - function that gives you the index of a key.
+ * @key: The key.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index at which the key/value should be stored
+ * in the array of the hash table.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value, index;
+
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
+
+	return (index);
+}
